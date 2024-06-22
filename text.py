2bc111ef3fa8dcdab7e6f9e14da34d29d32a06f6
Matteo Caputo <matteo.caputo2014@gmail.com>
2024-06-22 16:18:06 +0200

    highlighting passed to testing branch

--- text.py
+++ text.py
@@ -42,14 +42,14 @@ def open_file():
         with open(file_path, 'r') as file:
             text_editor.delete(1.0, tk.END)
             text_editor.insert(1.0, file.read())
-            highlight_python_code()
+            
 
 def save_file():
     file_path  = filedialog.asksaveasfilename()
     if file_path:
         with open(file_path, 'w') as file:
             file.write(text_editor.get(1.0, tk.END))
-        highlight_python_code()
+        
 
 def switch_to_dark_mode():
     if text_editor.cget('bg') == 'black':
@@ -57,35 +57,5 @@ def switch_to_dark_mode():
     else:
         text_editor.configure(bg='black', fg='white')
 
-def highlight_python_code():
-    text  = text_editor.get(1.0, tk.END)
-    words  = re.split(r'\W+', text)
-    word_index  = 0
-    for line in range(int(text_editor.index('1.0').split('.')[0]) + 1):
-        for column in range(int(text_editor.index('1.0').split('.')[1]) + 1):
-            if column > 0:
-                text_editor.see(f'{line}.0')
-                text_editor.mark_set('insert', f'{line}.0')
-                break
-        for word in words:
-            word_index += 1
-            start_index  = f'{line}.{column}'
-            end_index  = f'{line}.{column+len(word)}'
-            if word.lower() in ['print', 'def', 'class', 'if', 'else', 'for', 'while', 'try', 'except', 'finally', 'import']:
-                text_editor.tag_add('python', start_index, end_index)
-            elif word.lower() in ['true', 'false', 'None']:
-                text_editor.tag_add('boolean', start_index, end_index)
-            elif word.lower() in ['str', 'int', 'float', 'list', 'dict']:
-                text_editor.tag_add('type', start_index, end_index)
-
-text_editor.tag_config('python', foreground='blue')
-text_editor.tag_config('boolean', foreground='green')
-text_editor.tag_config('type', foreground='purple')
-
-def update_highlight():
-    highlight_python_code()
-    root.after(1, update_highlight) 
-
-update_highlight()
 
 root.mainloop()
